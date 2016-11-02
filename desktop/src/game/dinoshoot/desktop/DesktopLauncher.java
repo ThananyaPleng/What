0574f4d77257301ff02e4abfbb42c9910029a4e1
ThananyaPleng <s7070048@kmitl.ac.th>
2016-11-02 21:50:06 +0700

    add from before commit
    before not complete (now complete!!)

--- desktop/src/game/dinoshoot/desktop/DesktopLauncher.java
+++ desktop/src/game/dinoshoot/desktop/DesktopLauncher.java
@@ -2,15 +2,11 @@ package game.dinoshoot.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import game.dinoshoot.DinoShoot;
+import game.dinoshoot.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Dino Shoot";
-		config.width = 600;
-		config.height = 700;
-		config.useGL30 = true;
-		new LwjglApplication(new DinoShoot(), config);
+		new LwjglApplication(new Game(), config);
 	}
 }
